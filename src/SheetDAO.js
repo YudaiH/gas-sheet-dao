20cb72b1b302c0a584b2a374e1871d2c940c1320
Yudai Hara <yudaih1994@gmail.com>
2026-02-22 18:39:46 +0900

    Squashed commit of the following:
    commit 5ae059defd465343fab77f65b6ffdfa05be6b4ff
    Author: Yudai Hara <yudaih1994@gmail.com>
    Date:   Sun Feb 22 18:39:12 2026 +0900
    
        fix: integrate duplicated create()

--- src/SheetDAO.js
+++ src/SheetDAO.js
@@ -1,15 +1,4 @@
 /**
- * SheetDAOのインスタンスを作成します。
- * 外部プロジェクトからライブラリとして使用する場合、この関数を経由して呼び出します。
- * @param {string} sheetId スプレッドシートのID
- * @param {string} sheetName 対象のシート名
- * @return {SheetDAO} SheetDAOインスタンス
- */
-function create(sheetId, sheetName) {
-  return new SheetDAO(sheetId, sheetName);
-}
-
-/**
  * SheetDAO: Google Spread Sheet をDBのように操作するためのDAOクラス
  */
 class SheetDAO {
