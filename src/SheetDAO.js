49af2bf883632f45eb4c0beebd88a4876d075d7f
Yudai Hara <yudaih1994@gmail.com>
2026-02-22 17:13:53 +0900

    feat: add create function to publish as library

--- src/SheetDAO.js
+++ src/SheetDAO.js
@@ -1,4 +1,15 @@
 /**
+ * SheetDAOのインスタンスを作成します。
+ * 外部プロジェクトからライブラリとして使用する場合、この関数を経由して呼び出します。
+ * @param {string} sheetId スプレッドシートのID
+ * @param {string} sheetName 対象のシート名
+ * @return {SheetDAO} SheetDAOインスタンス
+ */
+function create(sheetId, sheetName) {
+  return new SheetDAO(sheetId, sheetName);
+}
+
+/**
  * SheetDAO: Google Spread Sheet をDBのように操作するためのDAOクラス
  */
 class SheetDAO {
